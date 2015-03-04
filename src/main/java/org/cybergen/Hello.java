efc056f2f336041d12321910d071ca5f61b853a7
vishnu prasad <vishnu667@gmail.com>
2015-03-04 10:54:21 +0530

    Initial commit

---
+++ src/main/java/org/cybergen/Hello.java
@@ -0,0 +1,10 @@
+package org.cybergen;
+
+/**
+ * Created by vishnu on 4/3/15.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Starting Java Application");
+    }
+}
---
+++ src/main/scala/org/cybergen/App.scala
@@ -0,0 +1,12 @@
+package org.cybergen
+
+/**
+ * Boot Strap Class
+ *
+ */
+object App 
+{
+    def main(args: Array[String]):Unit={
+        println( "Starting Application" )
+    }
+}
