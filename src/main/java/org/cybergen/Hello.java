1d7ccd6058592a97f141835dcec47956c0413dad
vishnu prasad <vishnu667@gmail.com>
2015-04-14 21:14:12 +0530

    basic clustering

--- src/main/java/org/cybergen/Hello.java
+++ src/main/java/org/cybergen/Hello.java
@@ -1,10 +1,41 @@
 package org.cybergen;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.mllib.clustering.KMeans;
+import org.apache.spark.mllib.clustering.KMeansModel;
+import org.apache.spark.mllib.linalg.Vector;
+import org.apache.spark.mllib.linalg.Vectors;
+
 /**
  * Created by vishnu on 4/3/15.
  */
 public class Hello {
     public static void main(String[] args){
         System.out.println("Starting Java Application");
+        SparkConf conf = new SparkConf().setAppName("K-means Example").setMaster("spark://cyborg:7077");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        String path = "/opt/spark/spark-1.2.1-bin-hadoop2.4/data/mllib/kmeans_data.txt";
+        JavaRDD<String> data = sc.textFile(path);
+        JavaRDD<Vector> parsedData = data.map(
+                new Function<String, Vector>() {
+                    public Vector call(String s) {
+                        String[] sarray = s.split(" ");
+                        double[] values = new double[sarray.length];
+                        for (int i = 0; i < sarray.length; i++)
+                            values[i] = Double.parseDouble(sarray[i]);
+                        return Vectors.dense(values);
+                    }
+                }
+        );
+        parsedData.cache();
+        
+        int numClusters = 2 ;
+        int numIterations = 20;
+        KMeansModel clusters = KMeans.train(parsedData.rdd(), numClusters, numIterations);
+        double WSSSE = clusters.computeCost(parsedData.rdd());
+        System.out.println("Within Set Sum of Squared Errors = " + WSSSE);
     }
 }
--- src/main/scala/org/cybergen/App.scala
+++ src/main/scala/org/cybergen/App.scala
@@ -1,12 +1,31 @@
 package org.cybergen
 
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
- * Boot Strap Class
- *
- */
-object App 
+* Boot Strap Class
+*
+*/
+object App
 {
     def main(args: Array[String]):Unit={
         println( "Starting Application" )
+        import org.apache.spark.mllib.clustering.KMeans
+        import org.apache.spark.mllib.linalg.Vectors
+        val conf = new SparkConf().setAppName("kmeansScala").setMaster("spark://cyborg:7077")
+          .setJars(Array("/home/vishnu/projects/spark-example/target/spark-example-1.0-SNAPSHOT-driver.jar"))
+        val sc = new SparkContext(conf)
+        // Load and parse the data
+        val data = sc.textFile("/opt/spark/spark-1.2.1-bin-hadoop2.4/data/mllib/kmeans_data.txt")
+        val parsedData = data.map(s => Vectors.dense(s.split(' ').map(_.toDouble))).cache()
+
+        // Cluster the data into two classes using KMeans
+        val numClusters = 2
+        val numIterations = 20
+        val clusters = KMeans.train(parsedData, numClusters, numIterations)
+
+        // Evaluate clustering by computing Within Set Sum of Squared Errors
+        val WSSSE = clusters.computeCost(parsedData)
+        println("Within Set Sum of Squared Errors = " + WSSSE)
     }
 }
