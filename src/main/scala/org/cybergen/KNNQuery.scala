ca57821e6a60921ae7aec0a639e73fbfb9268fec
vishnu prasad <vishnu667@gmail.com>
2015-04-18 20:13:35 +0530

    nearest neighbour search

--- src/main/scala/org/cybergen/KNNQuery.scala
+++ src/main/scala/org/cybergen/KNNQuery.scala
@@ -1,8 +1,19 @@
 package org.cybergen
 
+import org.apache.spark.{SparkContext, SparkConf}
+
 /**
  * Created by vishnu on 17/4/15.
  */
 object KNNQuery {
+  def main(args: Array[String]) :Unit = {
+    val conf = new SparkConf().setAppName("Nearest neighbour")
+      .setMaster("spark://cyborg:7077")
+      .setJars(Array("/home/vishnu/projects/spark-example/target/spark-example-1.0-SNAPSHOT-driver.jar"))
 
+    val sc = new SparkContext(conf)
+
+    val data = sc.textFile("/opt/spark/current/data/mllib/kmeans_data.txt")
+
+  }
 }
