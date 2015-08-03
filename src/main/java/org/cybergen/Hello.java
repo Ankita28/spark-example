27029428fca42079c4e01adfaa7f62eda2838621
vishnu prasad <vishnu667@gmail.com>
2015-08-03 15:41:44 +0530

    jaa sc initializing example

--- src/main/java/org/cybergen/Hello.java
+++ src/main/java/org/cybergen/Hello.java
@@ -1,13 +1,8 @@
 package org.cybergen;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.mllib.clustering.KMeans;
-import org.apache.spark.mllib.clustering.KMeansModel;
-import org.apache.spark.mllib.linalg.Vector;
-import org.apache.spark.mllib.linalg.Vectors;
+
 
 /**
  * Created by vishnu on 4/3/15.
@@ -16,27 +11,11 @@ import org.apache.spark.mllib.linalg.Vectors;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Starting Java Application");
-        SparkConf conf = new SparkConf().setAppName("K-means Example").setMaster("spark://cyborg:7077");
-        JavaSparkContext sc = new JavaSparkContext(conf);
-        String path = "/opt/spark/spark-1.2.1-bin-hadoop2.4/data/mllib/kmeans_data.txt";
-        JavaRDD<String> data = sc.textFile(path);
-        JavaRDD<Vector> parsedData = data.map(
-                new Function<String, Vector>() {
-                    public Vector call(String s) {
-                        String[] sarray = s.split(" ");
-                        double[] values = new double[sarray.length];
-                        for (int i = 0; i < sarray.length; i++)
-                            values[i] = Double.parseDouble(sarray[i]);
-                        return Vectors.dense(values);
-                    }
-                }
-        );
-        parsedData.cache();
+        SparkConf conf = new SparkConf().setAppName("").setMaster("local");
 
-        int numClusters = 2;
-        int numIterations = 20;
-        KMeansModel clusters = KMeans.train(parsedData.rdd(), numClusters, numIterations);
-        double WSSSE = clusters.computeCost(parsedData.rdd());
-        System.out.println("Within Set Sum of Squared Errors = " + WSSSE);
+        //sparkContext is being initialized
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        System.out.println("Within Set Sum of Squared Errors = ");
     }
 }
